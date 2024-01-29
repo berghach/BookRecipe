a319a7018af9dac86a522b5bb0a769b5f9a7d1a6
berghach <berghach.chemseddine@gmail.com>
2024-01-29 17:31:45 +0000

    avancer au CRUD

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+include("resources/views/welcome.blade.php");
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\recipeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recipes', [recipeController::class,'index'])->name('recipe.index');
